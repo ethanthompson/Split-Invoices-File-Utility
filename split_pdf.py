5ffca6439c6e8d027f500591b7e678dfa0cf1cd1
Ethan Thompson <ethanthompson@users.noreply.github.com>
2023-03-11 23:04:32 -0500

    Added support for filename update
    The filename is now a concatenated version of the sale ID, invoice ID, and account name.

--- split_pdf.py
+++ split_pdf.py
@@ -1,9 +1,14 @@
 import fitz
 import os
 import glob
+import shutil
 
 # Create our output directory
-os.mkdir('output')
+if os.path.exists('output'):
+  shutil.rmtree('output',ignore_errors=True)
+  os.mkdir('output')
+else:
+  os.mkdir('output')
 
 filePrefix = "invoice"
 
@@ -26,6 +31,7 @@ pageGroups = []
 
 # Iterate over the pages in the input PDF
 for page in pdf:
+  page.wrap_contents()
   # Check if the search term appears on the page
   if searchTerm in page.get_text("text"):
     # If search text is found, save the page number
@@ -35,19 +41,29 @@ numOfLoops = len(pageGroups)
 i = 0
 
 while (i < numOfLoops):
-  print(i)
 
   if(i == 0):
     startingPage = 0
     endingPage = pageGroups[i]
-    print(startingPage, endingPage)
+    # print(startingPage, endingPage)
   else:
     startingPage = pageGroups[i - 1] + 1
     endingPage = pageGroups[i]
-    print(startingPage, endingPage)
+    # print(startingPage, endingPage)
+  
+  # This page is the one you want to scrape for the filename.
+  metadataPage = pdf[startingPage]
+
+  # Grab the "metadata" from the first page. Use the hack mentioned on line 62 to make finding data a bit easier.
+  saleNum = metadataPage.get_text("text",clip=fitz.Rect(125,321,150,521))[:-3] # Trim the string to get ride of the /X num at the end. Not sure what that digit even represents.
+  accountName = metadataPage.get_text("text",clip=fitz.Rect(163,221,178,461))[:-1] # Trim the string to get ride of the newline character
+  invoiceNum = metadataPage.get_text("text",clip=fitz.Rect(125,30,140,150))[:-1] # Trim the string to get ride of the newline character
+  
+  # Drawing below left for debugging purposes. To make coordinates easier to identify, rotate the pdfs 90 degrees counter clock-wise first. For some reason the 0,0 origin is in the top right instead of the top left.
+  # invoiceNumDraw = metadataPage.add_rect_annot(fitz.Rect(125,30,140,150))
 
   newPDF = pdf.convert_to_pdf(from_page=startingPage, to_page=endingPage, rotate=0)
   newPDF = fitz.open("pdf",newPDF)
-  newPDF.save("output/" + filePrefix + "_" + str(i) + ".pdf")
+  newPDF.save("output/" + saleNum + "_" + invoiceNum + "_" + accountName + ".pdf")
 
   i+=1
